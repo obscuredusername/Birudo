0478f1ef4aedcded1cd93c0292c11ba96458a68a
obscuredusername <zubayrr1011@gmail.com>
2023-05-14 18:10:57 +0500

    good job

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import mainv from './components/mainv.vue'
+</script>
+
+<template>
+  <link rel="icon" type="image/x-icon" href="/public/favicon.ico">
+  <mainv></mainv>
+  
+
+  
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;300&display=swap');
+
+</style>
